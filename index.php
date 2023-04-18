398f3712f4c72072e3461f36a9a71f1390cb933b
Aman07a <aman16.aa17@gmail.com>
2023-04-18 08:41:29 +0200

    Starting Our Multiple Choice Block Type

--- index.php
+++ index.php
@@ -8,31 +8,43 @@
  Author URI: https://www.udemy.com/user/aman-574/
 */
 
-if (!defined("ABSPATH")) exit; // Exit if accessed directly
+if (!defined("ABSPATH")) {
+    exit();
+} // Exit if accessed directly
 
 class AreYouPayingAttention
 {
     function __construct()
     {
-        add_action("init", array($this, "adminAssets"));
+        add_action("init", [$this, "adminAssets"]);
     }
 
     function adminAssets()
     {
-        wp_register_script("ournewblocktype", plugin_dir_url(__FILE__) . "build/index.js", array("wp-blocks", "wp-element"));
-        register_block_type("ourplugin/are-you-paying-attention", array(
+        wp_register_style(
+            "quizeditcss",
+            plugin_dir_url(__FILE__) . "build/index.css"
+        );
+        wp_register_script(
+            "ournewblocktype",
+            plugin_dir_url(__FILE__) . "build/index.js",
+            ["wp-blocks", "wp-element", "wp-editor"]
+        );
+        register_block_type("ourplugin/are-you-paying-attention", [
             "editor_script" => "ournewblocktype",
-            "render_callback" => array($this, "theHTML")
-        ));
+            "editor_style" => "quizeditcss",
+            "render_callback" => [$this, "theHTML"],
+        ]);
     }
 
     function theHTML($attributes)
     {
         ob_start(); ?>
-        <h3>Today the sky is
-            <?php echo esc_html($attributes["skyColor"]) ?>
+        <h3>
+            Today the sky is
+            <?php echo esc_html($attributes["skyColor"]); ?>
             and the grass is
-            <?php echo esc_html($attributes["grassColor"]) ?>
+            <?php echo esc_html($attributes["grassColor"]); ?>
             !
         </h3>
 <?php return ob_get_clean();
